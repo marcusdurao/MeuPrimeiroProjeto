214a38be08e8d5a47c71dc0def398542f79135ab
Marcus Durao <mvdurao@gmail.com>
2018-09-19 11:19:59 -0300

    teste

--- src/main/java/com/br/sisped/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/br/sisped/MeuPrimeiroProjetoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("teste");
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 	}
 }
